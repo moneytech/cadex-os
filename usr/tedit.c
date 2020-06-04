b1ed5667d4f9895b3f6fcb2c7346713808f97f86
asiawin <aswindeepak40@gmail.com>
2020-06-04 14:09:34 +0530

    Updated files

---
+++ usr/tedit.c
@@ -0,0 +1,51 @@
+/*
+Copyright (C) 2019-2020 OpenCreeck
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+/*
+A trivial user level program to try out basic system calls.
+*/
+
+#include "library/syscalls.h"
+#include "library/string.h"
+#include "library/user-io.h"
+
+int main(int argc, char *argv[])
+{
+	uint32_t j = 0;
+	syscall_chdir("/");
+	printf("Got root access\n");
+	int fd = syscall_open_file("kevin.txt", 1, 0);
+	printf("Got filesystem access %d\n", fd);
+	printf("writing to file...\n");
+	for(;;) {
+		j++;
+		char buffer[100] = "Hello, world! I can write to ";
+		char num[5];
+		char newline[2] = "\n";
+		int n;
+		uint_to_string(j, num);
+		strcat(buffer, num);
+		strcat(buffer, newline);
+		n = syscall_object_write(fd, buffer, strlen(buffer));
+		if(n < 0)
+			break;
+		printf("wrote %d chars: %s\n", n, buffer);
+	}
+	syscall_object_close(fd);
+	fd = syscall_open_file("kevin.txt", 1, 0);
+	char buffer[1000];
+	int n;
+	printf("Reading from file...\n");
+	while((n = syscall_object_read(fd, buffer, 999)) > 0) {
+		buffer[n] = 0;
+		printf("%s");
+		flush();
+	}
+	syscall_object_close(fd);
+	syscall_process_exit(0);
+
+	return 0;
+}
