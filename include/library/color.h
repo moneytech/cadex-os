7d6128b78ccf1ac73585d236cd157065ab513eef
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-06-27 17:01:14 +0530

    Removed CI

--- include/library/color.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLOR_H
-#define COLOR_H
-#define BLUE 60,60,150
-#define BLACK 0,0,0
-#define WHITE 255,255,255
-#define GREEN 0,255,0
-#define RED 255,0,0
-#define GRAY 230,230,230
-#endif
