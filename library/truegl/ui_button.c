8aaef6770e1feb4bf4ce3e7ad414a44d4a2cc186
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-08-26 16:42:40 +0530

    Create ui_button.c

---
+++ library/truegl/ui_button.c
@@ -0,0 +1,13 @@
+/**
+ * TrueGL Graphics Library v0.2
+ * 
+ * Copyright (C) 2019-2020 OpenCreeck
+ * This software is distributed under the GNU General Public License.
+ * See the file LICENSE for details.
+*/
+
+#include <truegl/truegl.h>
+
+int drawButton(int x, int y, int w, int h, char *label){
+    
+}
