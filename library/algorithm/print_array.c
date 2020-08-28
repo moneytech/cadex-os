08355a8fba92278c8852678cbab3546410c796f6
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-08-28 16:51:41 +0530

    Added algorithm library functions

---
+++ library/algorithm/print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+/**
+ * Prints an array recursively within a given range
+*/
+void print_array(int arr[], int start, int len)
+{
+    /* Recursion base condition */
+    if(start >= len)
+        return;
+    
+    /* Prints the current array element */
+    printf("%d, ", arr[start]);
+
+    /* Recursively call print_array to print next element in array */
+    print_array(arr, start - 1, len);
+}
--- library/ctime.c
+++ library/ctime.c
@@ -3,8 +3,7 @@ Copyright (C) 2019-2020 OpenCreeck
 This software is distributed under the GNU General Public License.
 See the file LICENSE for details.
 */
-#include <time.h>
-#include <sys/time.h>
+#include <library/types.h>
 
 char * ctime(const time_t * timep) {
     return asctime(localtime(timep));
