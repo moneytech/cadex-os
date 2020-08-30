d66c4307828ef11e9211db8aaf01c7a6b15a5409
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-08-30 19:09:49 +0530

    update

---
+++ include/security.h
@@ -0,0 +1,13 @@
+#ifndef SECURITY_H
+#define SECURITY_H
+
+#include <stdio.h>
+#include <library/scanf.h>
+
+typedef struct sec_object{
+
+} sec_object_t;
+int get_proc_uid();
+int get_sudo_permission();
+
+#endif // !SECURITY_H
