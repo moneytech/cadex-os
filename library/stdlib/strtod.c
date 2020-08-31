07298770aa575673573b5b7b8bc044962611222f
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-08-31 22:34:20 +0530

    updated libraries

--- library/stdlib/strtod.c
+++ library/stdlib/strtod.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
-#include "shgetc.h"
-#include "floatscan.h"
-#include "stdio_impl.h"
+// #include "shgetc.h"
+// #include "floatscan.h"
+// #include "stdio_impl.h"
 
-static long double strtox(const char *s, char **p, int prec)
-{
-	FILE f;
-	sh_fromstring(&f, s);
-	shlim(&f, 0);
-	long double y = __floatscan(&f, prec, 1);
-	off_t cnt = shcnt(&f);
-	if (p) *p = cnt ? (char *)s + cnt : (char *)s;
-	return y;
-}
+// static long double strtox(const char *s, char **p, int prec)
+// {
+// 	FILE f;
+// 	sh_fromstring(&f, s);
+// 	shlim(&f, 0);
+// 	long double y = __floatscan(&f, prec, 1);
+// 	off_t cnt = shcnt(&f);
+// 	if (p) *p = cnt ? (char *)s + cnt : (char *)s;
+// 	return y;
+// }
 
 float strtof(const char *restrict s, char **restrict p)
 {
@@ -29,9 +29,9 @@ long double strtold(const char *restrict s, char **restrict p)
 	return strtox(s, p, 2);
 }
 
-weak_alias(strtof, strtof_l);
-weak_alias(strtod, strtod_l);
-weak_alias(strtold, strtold_l);
-weak_alias(strtof, __strtof_l);
-weak_alias(strtod, __strtod_l);
-weak_alias(strtold, __strtold_l);
+// weak_alias(strtof, strtof_l);
+// weak_alias(strtod, strtod_l);
+// weak_alias(strtold, strtold_l);
+// weak_alias(strtof, __strtof_l);
+// weak_alias(strtod, __strtod_l);
+// weak_alias(strtold, __strtold_l);
