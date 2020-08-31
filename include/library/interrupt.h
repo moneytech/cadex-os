b7408055999c9b31dff5a392a358812e97e0eb02
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-08-31 22:00:13 +0530

    Added some libraries from musl libc

--- include/library/interrupt.h
+++ include/library/interrupt.h
@@ -2,12 +2,9 @@
 #define INTERRUPT_H
 
 #include <kernel/types.h>
+#include <stdio.h>
 
-uint8_t inb(uint16_t port);
-void outb(uint16_t port, uint8_t data);
-void wait_for_io(uint32_t timer_count);
-void sleep(uint32_t timer_count);
-static void play_sound(uint32_t nFrequence);
-static void nosound();
-void beep();
+void interrupt_disable(int i);
+void interrupt_enable(int i);
+
 #endif
