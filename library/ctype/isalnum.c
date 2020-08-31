abd26a70f7c3b2c3cba6bd91b4f53f38e09ec5bc
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-08-31 22:27:59 +0530

    updated files

--- library/ctype/isalnum.c
+++ library/ctype/isalnum.c
@@ -5,9 +5,4 @@ int isalnum(int c)
 	return isalpha(c) || isdigit(c);
 }
 
-int __isalnum_l(int c, locale_t l)
-{
-	return isalnum(c);
-}
-
-weak_alias(__isalnum_l, isalnum_l);
+// weak_alias(__isalnum_l, isalnum_l);
--- library/ctype/isblank.c
+++ library/ctype/isblank.c
@@ -1,13 +1,7 @@
 #include <ctype.h>
+#undef isblank 
 
 int isblank(int c)
 {
 	return (c == ' ' || c == '\t');
 }
-
-int __isblank_l(int c, locale_t l)
-{
-	return isblank(c);
-}
-
-weak_alias(__isblank_l, isblank_l);
