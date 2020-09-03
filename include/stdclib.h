3e84f260f3cff9d0f7561238b7cd3139d4979fa9
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-03 16:49:55 +0530

    Update stdclib.h

--- include/stdclib.h
+++ include/stdclib.h
@@ -5,8 +5,13 @@
 #include <library/malloc.h>
 #include <library/version.h>
 
-#define __c_attrtib __INSERT_CDX_ATTRIBUTE()
+// some cadex-specific attributes
+#define __cdx_attrtib __INSERT_CDX_ATTRIBUTE()
 
+// cadex_version.c
 char *get_system_version();
 
+// contty.c
+int get_console_width(int window);
+int get_console_height(int window);
 #endif // !STDCLIB_H
