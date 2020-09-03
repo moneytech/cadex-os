0f9b7de6a7790d2f2e0c4c4263626c66fd076b7e
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-03 16:49:53 +0530

    Update security.h

--- include/security.h
+++ include/security.h
@@ -5,7 +5,9 @@
 #include <library/scanf.h>
 
 typedef struct sec_object{
-
+    int proc_uid;
+    int call_obj_id;
+    char *proc_exe_name;
 } sec_object_t;
 int get_proc_uid();
 int get_sudo_permission();
