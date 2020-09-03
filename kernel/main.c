46afe8ff9b4ef339369ef06951ec48a2624433df
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-03 20:28:58 +0530

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -29,7 +29,7 @@ See the file LICENSE for details.
 #include <sysinfo.h>
 
 /*
-This is the C initialization point of the Cadex kernel.
+This is the C initialization point of the Cadex OS.
 By the time we reach this point, we are in protected mode,
 with interrupts disabled, a valid C stack, but no malloc heap.
 Now we initialize each subsystem in the proper order:
@@ -40,29 +40,50 @@ int kernel_main()
 	char a;
 	struct graphics *g = graphics_create_root();
 
+	// init the console
 	console_init(g);
+	// add reference to console_root
 	console_addref(&console_root);
-
+	
+	// print the screen resolution
 	printf("[SYS] video: %d x %d\n", video_xres, video_yres, video_xbytes);
+	// print the kernel size 
 	printf("[SYS] kernel: %d bytes\n", kernel_size);
 
+	// init paging
 	page_init();
+	// init kernel memory allocator
 	kmalloc_init((char *) KMALLOC_START, KMALLOC_LENGTH);
+	// init interrupt manager
 	interrupt_init();
+	// init realtime clock
 	rtc_init();
+	// init system clock
 	clock_init();
+	// init mouse
 	mouse_init();
+	// init keyboard
 	keyboard_init();
+	// init process manager
 	process_init();
+	// init ata driver
 	ata_init();
+	// init atapi(cdrom) driver
 	cdrom_init();
+	// init filesystem driver
 	diskfs_init();
+	// init serialports
 	serial_init(0x3f8);
+	// mount atapi device with unit 2
 	kshell_mount("atapi", 2, "cdromfs");
-	printf("\n>>>>>> Cadex OS %s | beta-%s <<<<<<\n", SYS_VER, SYS_BUILD);
+	#ifdef DEBUG
+	printf("\nCadex OS %sb-%s <<<<<<\n", SYS_VER, SYS_BUILD); // #define debug to enable version info
+	#endif
+	// launch shell
 	kshell_launch();
 
 	while(1) {
+		// we need this for the kshell or it will just show text and we can't type anything into it
 		console_putchar(&console_root,keyboard_read(a));
 	}
 
