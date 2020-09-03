1fb86e4935c7b9efe1e7a2301a61fec74d139f9a
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-03 16:50:36 +0530

    Create contty.c

---
+++ libc/stdclib/contty.c
@@ -0,0 +1,20 @@
+/*
+Copyright (C) 2019-2020 OpenCreeck
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+/**
+ * contty.c: Console related functions
+ * 
+ * This file is a part of the Standard Cadex Library
+*/
+
+#include <stdclib.h>
+
+int get_console_width(int window){
+
+}
+int get_console_height(int window){
+
+}
