4e17b5a4d701f5407508ccd7054716c5e7930596
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-06 15:38:19 +0530

    added iostream to c++ libraries

---
+++ lib/io/istream.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void cin(char string[1024]){
+    scanf(string, sizeof(string));
+}
---
+++ lib/io/ostream.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Prints text to console
+ * @param string The string to print
+*/
+void cout(char *string){
+    printf("%s", string);
+}
