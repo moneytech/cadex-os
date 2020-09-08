ca6a9f7212f90a96d4378984e02d507bc699331a
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-08 18:40:37 +0530

    updated files

--- lib/WindowCompositor.cpp
+++ lib/WindowCompositor.cpp
@@ -2,20 +2,3 @@
  * This file is a part of GWC (Graphical Window Compositor) library
 */
 #include <graphics>
-
-class WindowCompositor
-{
-private:
-    /* data */
-public:
-    WindowCompositor(int window);
-    ~WindowCompositor();
-};
-
-WindowCompositor::WindowCompositor(int window)
-{
-}
-
-WindowCompositor::~WindowCompositor()
-{
-}
