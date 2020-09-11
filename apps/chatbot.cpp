6ab0465652eb2843fc87bdd510d8ab82f3a1b230
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-09-11 22:40:48 +0530

    Added a chatbot example

---
+++ apps/chatbot.cpp
@@ -0,0 +1,10 @@
+/**
+ * chatbot.cpp: An example demonstrating how to make Chat Bots with C++ libraries and APIs
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv[]){
+    Console::WriteLine("Chatbot app with C++ for Cadex OS");
+}
