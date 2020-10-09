ebaf0799499eeecfb0538f36e9ba7c195d6a9cbe
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-09 17:19:21 +0530

    Add stub body

--- bin/stapp.c
+++ bin/stapp.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2019-2020 OpenCreeck
  * This software is distributed under the GNU General Public License
  * See the file LICENSE for details
-*/
+ */
 
 /**
  * Startup Application
@@ -10,4 +10,15 @@
  * Startup Application is responsible for choosing
  * which application should start after the system
  * has booted up.
-*/
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int index;
+  int temp;
+  uint8_t buffer[1024];
+
+  // TODO: Implement stapp functionality
+
+  return 0;
+}
