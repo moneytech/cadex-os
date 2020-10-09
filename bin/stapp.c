010cd84501fdbc45bea25c9e155c4960debea663
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-09 16:52:01 +0530

    Create stapp.c

---
+++ bin/stapp.c
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2019-2020 OpenCreeck
+ * This software is distributed under the GNU General Public License
+ * See the file LICENSE for details
+*/
+
+/**
+ * Startup Application
+ *
+ * Startup Application is responsible for choosing
+ * which application should start after the system
+ * has booted up.
+*/
