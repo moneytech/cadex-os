569bf630f46fd08c7800092e1a1173ec5dfa1909
HyperCreeck <aswindeepak40@gmail.com>
2020-10-10 12:38:23 +0530

    kshell.h: Add struct kshell_t for storing kshell metadata

--- kernel/kshell.h
+++ kernel/kshell.h
@@ -7,6 +7,16 @@ See the file LICENSE for details.
 #ifndef KSHELL_H
 #define KSHELL_H
 
+typedef struct
+{
+    char *device_name;
+    int kshell_prompt_index;
+    char *current_directory;
+    char *custom_kshell_command;
+    int last_run_proc_exitcode;
+    int last_process_run;
+} kshell_t;
+
 int kshell_launch();
 
 #endif
