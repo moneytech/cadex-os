6d3ab8940a50d96d6659bb7f41a14eeeccd03170
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:57:49 +0530

    Update printer.c

--- usr.bin/examples/printer.c
+++ usr.bin/examples/printer.c
@@ -8,28 +8,24 @@ See the file LICENSE for details.
 A fun graphics demo that features a line segment bouncing around the screen.
 */
 
-#include "library/stdio.h"
-#include "library/string.h"
-#include "library/syscalls.h"
+#include <stdio.h>
 
-#define WIDTH (200)
+#define WIDTH  (200)
 #define HEIGHT (200)
 
-int main(int argc, char* argv[])
-{
-    renderWindow(WN_STDWINDOW);
-    setTextColor(0, 0, 255,0);
-    clearScreen(0, 0, WIDTH, HEIGHT);
-    flush();
+int main(int argc, char *argv[]) {
+    render_window(WN_STDWINDOW);
+    setTextColor(0, 0, 255, 0);
+    clear_screen();
 
-    int cd = syscall_open_console(WN_STDWINDOW);
-    if (cd < 0) {
+    int console = syscall_open_console(WN_STDWINDOW);
+    if (console < 0) {
         printf("Console open failed!\n");
         return 2;
     }
-    syscall_object_dup(cd, KNO_STDOUT);
 
-    printf("hello world!\n");
+    syscall_object_dup(console, STDOUT);
+    printf("Hello World!\n");
 
     return 0;
 }
