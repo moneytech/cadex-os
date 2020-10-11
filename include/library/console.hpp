01c67367aab82c2e790fe807e3536e6e4f1c2b04
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:57:07 +0530

    Update console.hpp

--- include/library/console.hpp
+++ include/library/console.hpp
@@ -2,11 +2,11 @@
  * Copyright (C) 2019-2020 OpenCreeck
  * This software is distributed under the GNU General Public License
  * See the file LICENSE for details
-*/
+ */
 
 /**
  * This file contains the class definitions of the Console class
-*/
+ */
 #ifndef _CONSOLE_HH
 #define _CONSOLE_HH
 
@@ -23,125 +23,96 @@
 namespace std {
 
 /**
-     * The base console class
-     * @author HyperCreeck (@asiawin)
-    */
+ * The base console class
+ * @author HyperCreeck (@asiawin)
+ */
 class Console {
-public:
+    public:
     /**
-         * Create a console on WN_STDWINDOW
-        */
-    Console()
-    {
-        renderWindow(WN_STDWINDOW);
-    }
+     * Create a console on WN_STDWINDOW
+     */
+    Console() { render_window(WN_STDWINDOW); }
 
     /**
-         * Creates a console on specified window
-         * @param window The window to initialize the console
-        */
-    Console(int window)
-    {
-        renderWindow(window);
-    }
+     * Creates a console on specified window
+     * @param window The window to initialize the console
+     */
+    Console(int window) { render_window(window); }
 
     /**
-         * Deletes the console object
-        */
-    ~Console()
-    {
-    }
+     * Deletes the console object
+     */
+    ~Console() {}
 
     /**
-         * Writes the specified string to the console
-         * @param str The string to write to the console
-        */
-    static void Write(char* str)
-    {
-        printf("%s", str);
-    }
+     * Writes the specified string to the console
+     * @param str The string to write to the console
+     */
+    static void Write(char *str) { printf("%s", str); }
 
     /**
-         * Writes the specified string to the console
-         * @param str The string to write to the console
-        */
-    static auto Write(String* str) -> void
-    {
-        char* tmp = str->getValue();
+     * Writes the specified string to the console
+     * @param str The string to write to the console
+     */
+    static auto Write(String *str) -> void {
+        char *tmp = str->getValue();
         printf("%s", tmp);
     }
 
     /**
-         * Writes a line to the console
-         * @param str The string to write to the console
-        */
-    static auto WriteLine(String* str) -> void
-    {
-        char* tmp = str->getValue();
+     * Writes a line to the console
+     * @param str The string to write to the console
+     */
+    static auto WriteLine(String *str) -> void {
+        char *tmp = str->getValue();
         printf("%s", tmp);
     }
 
     /**
-         * Writes a line to console
-         * @param str String to write to console
-        */
-    static void WriteLine(char* str)
-    {
-        printf("%s\n", str);
-    }
+     * Writes a line to console
+     * @param str String to write to console
+     */
+    static void WriteLine(char *str) { printf("%s\n", str); }
 
     /**
-         * Read a line from the STDIN
-         * @param str The string to store the input to
-        */
-    static void ReadLine(char* str)
-    {
-        scanf(str, sizeof(str));
-    }
+     * Read a line from the STDIN
+     * @param str The string to store the input to
+     */
+    static void ReadLine(char *str) { scanf(str, sizeof(str)); }
 
     /**
-         * Sets the foreground color of the console
-         * @param color The color to use
-        */
-    static auto SetForegroundColor(Colors::Color* color) -> void
-    {
+     * Sets the foreground color of the console
+     * @param color The color to use
+     */
+    static auto SetForegroundColor(Colors::Color *color) -> void {
         setTextColor(color->r, color->g, color->b, color->a);
     }
 
     /**
-         * Prints a character to the console
-         * @param value The value to print to the console
-        */
-    static auto PutChar(char value) -> void
-    {
-        printf_putchar(value);
-    }
+     * Prints a character to the console
+     * @param value The value to print to the console
+     */
+    static auto PutChar(char value) -> void { printf_putchar(value); }
 
     /**
-         * Prints an integer value to the console
-         * @param value The value to print to the console
-        */
-    static auto PutInt(int value) -> void
-    {
-        printf("%d", value);
-    }
+     * Prints an integer value to the console
+     * @param value The value to print to the console
+     */
+    static auto PutInt(int value) -> void { printf("%d", value); }
 
     /**
-         * Prints a hexadecimal value to the console
-         * @param value The value to print to the console
-        */
-    static auto PutHex(uint32_t value) -> void
-    {
-        printf("%x", value);
-    }
+     * Prints a hexadecimal value to the console
+     * @param value The value to print to the console
+     */
+    static auto PutHex(uint32_t value) -> void { printf("%x", value); }
 
     /**
-         * Reads data from the keyboard with the specified length
-         * @param output The variable to store the read data
-         * @param length The length of data (i.e keystrokes) to be read from the keyboard
-        */
-    static auto Read(void* output, int length) -> void
-    {
+     * Reads data from the keyboard with the specified length
+     * @param output The variable to store the read data
+     * @param length The length of data (i.e keystrokes) to be read from the
+     * keyboard
+     */
+    static auto Read(void *output, int length) -> void {
         read_object(STDIN, output, length);
     }
 };
