d3f31fcf85f43b39791fe5d1e5971f7a84de4689
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:57:47 +0530

    Update color.c

--- usr.bin/color.c
+++ usr.bin/color.c
@@ -6,14 +6,13 @@ Simple application to change color of text.
 #include <library/string.h>
 #include <library/types.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     int dims[2];
     syscall_object_size(WN_STDWINDOW, dims, 2);
 
     int width = dims[0];
     int height = dims[1];
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     // clearScreen(0, 0, width, height);
     if (!strcmp(argv[0], "blue")) {
         setTextColor(BLUE, 0);
