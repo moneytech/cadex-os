34909d6dfcba95886d22a4016f90cbff515dbf31
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:58:06 +0530

    Update splash.c

--- usr.bin/splash.c
+++ usr.bin/splash.c
@@ -1,8 +1,7 @@
 #include <library/scanf.h>
 #include <stdio.h>
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char *argv[]) {
     int x1 = 12;
     int y1 = 12;
     int dims[2];
@@ -13,61 +12,61 @@ int main(int argc, const char* argv[])
     int width = dims[0];
     int height = dims[1];
 
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     clearScreen(0, 0, width, height);
     flushScreen();
     flush();
     draw_cadex_logo(130, 20);
     drawRect(80, 80, 10, 10);
     print(85, 100, "Loading components...");
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     sleepThread(1000);
     drawRect(80, 80, 20, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     print(85, 100, "Loading /sys/manager.config...");
     sleepThread(1000);
     drawRect(80, 80, 30, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     print(85, 100, "Loading /etc/cash.config...    ");
     sleepThread(1000);
     drawRect(80, 80, 40, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     print(85, 100, "          All set!           ");
     sleepThread(1000);
     drawRect(80, 80, 50, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     drawRect(80, 80, 60, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     sleepThread(1000);
     drawRect(80, 80, 70, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     sleepThread(1000);
     drawRect(80, 80, 80, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     sleepThread(1000);
     drawRect(80, 80, 90, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     sleepThread(1000);
     drawRect(80, 80, 100, 10);
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     flushScreen();
     flush();
     sleepThread(1000);
