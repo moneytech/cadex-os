49e455e482857b8673d77ef91ba051dcd50463a1
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:57:59 +0530

    Update matrix.c

--- usr.bin/matrix.c
+++ usr.bin/matrix.c
@@ -1,22 +1,21 @@
 #include <library/scanf.h>
 #include <stdio.h>
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char *argv[]) {
     int rows, cols, i, j, k;
     int dims[2];
     syscall_object_size(WN_STDWINDOW, dims, 2);
     int height = dims[1];
     cols = 128;
     rows = height;
-    renderWindow(WN_STDWINDOW);
+    render_window(WN_STDWINDOW);
     clear_screen();
     k = 1;
     while (1) {
         for (i = 1; i <= rows; i++) {
             for (j = 1; j <= cols; j++) {
                 if (k == 1) {
-                    renderWindow(WN_STDWINDOW);
+                    render_window(WN_STDWINDOW);
                     setTextColor(10, 200, 10, 100);
                     printf("1 ");
                 } else {
@@ -29,9 +28,9 @@ int main(int argc, const char* argv[])
                 k *= -1;
             }
             sleepThread(200);
-            //printf("\n");
+            // printf("\n");
         }
     }
 
-    //return 0;
+    // return 0;
 }
