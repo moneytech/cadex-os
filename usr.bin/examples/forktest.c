fa3ee575b0b0650c565b45801e4365e0493ce8e3
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:44:17 +0530

    examples/forktest.c: Update license header and fix formatting

--- usr.bin/examples/forktest.c
+++ usr.bin/examples/forktest.c
@@ -1,7 +1,12 @@
-#include "library/string.h"
-#include "library/syscalls.h"
+/**
+ * Copyright (C) 2019-2020 OpenCreeck
+ * This software is distributed under the GNU General Public License
+ * See the file LICENSE for details
+*/
 
-int main(int argc, char* argv[])
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
 {
     printf("hello world, I am %d.\n", syscall_process_self());
     int x = fork();
