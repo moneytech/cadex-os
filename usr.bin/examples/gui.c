ca3e11228fcfceb358457587403d3e86608e3664
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 13:21:19 +0530

    Create examples/gui.c

---
+++ usr.bin/examples/gui.c
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2019-2020 OpenCreeck
+ * This software is distributed under the GNU General Public License
+ * See the file LICENSE for details
+ */
+
+#include <stdio.h>
+#include <truegl/truegl.h>
+
+int main(int argc, char *argv[]) {
+    setup_window();
+    return 0;
+}
