e5209b65dcb4dd2d4f6fe396cd7f39ee562d06d8
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-11 12:57:36 +0530

    Update tgl_base.c

--- libc/truegl/tgl_base.c
+++ libc/truegl/tgl_base.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2019-2020 OpenCreeck
  * This software is distributed under the GNU General Public License.
  * See the file LICENSE for details.
-*/
+ */
 
 #include <debug.h>
 #include <truegl/events.h>
@@ -16,9 +16,8 @@
  * @param sprite Reference to the sprite to move. Must have a pointer type
  * @param x X position to move to
  * @param Y Y position to move to
-*/
-void move_sprite(Sprite* sprite, int x, int y)
-{
+ */
+void move_sprite(Sprite *sprite, int x, int y) {
     sprite->posX = x;
     sprite->posY = y;
 }
@@ -28,28 +27,21 @@ void move_sprite(Sprite* sprite, int x, int y)
  * @param sprite Reference to the sprite to resize. Must have a pointer type
  * @param width Width of the sprite to resize to
  * @param height Height of the sprite to resize to
-*/
-void resize_sprite(Sprite* sprite, int width, int height)
-{
+ */
+void resize_sprite(Sprite *sprite, int width, int height) {
     sprite->height = height;
     sprite->width = width;
 }
 
-void draw_pixel(int x, int y, int r, int g, int b, int a)
-{
+void draw_pixel(int x, int y, int r, int g, int b, int a) {
     setTextColor(r, g, b, a);
     drawRect(x, y, 1, 1);
 }
 
-int get_mouse_down()
-{
-    return 0;
-}
+int get_mouse_down() { return 0; }
 
-void setup_window()
-{
-    renderWindow(WN_STDWINDOW);
+void setup_window() {
+    render_window(WN_STDWINDOW);
     clear_screen();
     dbg_printf("<tgl> (tgl_base.c:53): setting up window...\n");
 }
-
