364ffd430725367cfc46d8b69dd7a0d6465b7849
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-13 16:08:25 +0530

    truegl/truegl.h: Fix C linkage problem

--- include/truegl/truegl.h
+++ include/truegl/truegl.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2019-2020 OpenCreeck
  * This software is distributed under the GNU General Public License.
  * See the file LICENSE for details.
-*/
+ */
 
 #include <library/_cheader.h>
 #include <library/color.h>
@@ -16,7 +16,7 @@
 /**
  * This enum is defined but not used. Stubbed
  */
-static enum UIColor {
+enum UIColor {
     T_COLOR_BLACK,
     T_COLOR_BLUE,
     T_COLOR_GREEN,
@@ -30,16 +30,15 @@ static enum UIColor {
 };
 /**
  * GameObject type definition
-*/
-typedef struct
-{
+ */
+typedef struct {
     int posX;
     int posY;
     int width;
     int height;
 } GameObject;
 
-typedef struct gui_color {
+typedef struct {
     int r;
     int g;
     int b;
@@ -48,21 +47,20 @@ typedef struct gui_color {
 
 /**
  * Sprite type definition
-*/
-typedef struct GameObject {
+ */
+typedef struct {
     int posX;
     int posY;
     int width;
     int height;
 } Sprite;
 
-typedef struct
-{
+typedef struct {
     int posX;
     int posY;
     int width;
     int height;
-    char* label;
+    char *label;
 } UIElement;
 
 typedef struct {
@@ -71,7 +69,7 @@ typedef struct {
     uint32_t height;
 
     /* Window backing buffer */
-    char* buffer;
+    char *buffer;
 
     /* Window focused flag */
     uint8_t focused;
@@ -81,12 +79,18 @@ typedef struct {
     int32_t y;
 
     /* Window border color */
-    gui_color_t* border_color;
+    gui_color_t *border_color;
+
+	/* Root window flag */
 } tgl_window_t;
 
 /* Get window and objects */
-#define get_object(w) ((struct GameObject*)w)
-#define get_window(w) ((tgl_window_t*)w)
+#define get_object(w) ((struct GameObject *)w)
+#define get_window(w) ((tgl_window_t *)w)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
  * Moves the specivied sprite to the specified X and Y location
@@ -94,20 +98,41 @@ typedef struct {
  * @param sprite Reference to the sprite to move. Must have a pointer type
  * @param x X position to move to
  * @param Y Y position to move to
-*/
-void move_sprite(Sprite* sprite, int x, int y);
+ */
+void move_sprite(Sprite *sprite, int x, int y);
 
 /**
  * Resises the specified sprite to the specified width and height
  * @param sprite Reference to the sprite to resize. Must have a pointer type
  * @param width Width of the sprite to resize to
  * @param height Height of the sprite to resize to
-*/
-void resize_sprite(Sprite* sprite, int width, int height);
+ */
+void resize_sprite(Sprite *sprite, int width, int height);
 
-int movable_scanf(int x, int y, char* line, int length);
+/* Internal function. Used as a base for other functions */
+int movable_scanf(int x, int y, char *line, int length);
+
+/**
+ * Draws a pixel at the specified position with the specified color
+ */
 void draw_pixel(int x, int y, int r, int g, int b, int a);
-void create_edittext(int x, int y, int w, int h, char* placeholder, char* line);
-int draw_button(int x, int y, int w, int h, char* label);
-int draw_static_Textarea(char* placeholder);
+void create_edittext(int x, int y, int w, int h, char *placeholder, char *line);
+
+/**
+ * Draws a graphical button with specified dimensions
+ */
+int draw_button(int x, int y, int w, int h, char *label);
+
+/**
+ * Draws a static textarea at the specified position with specified placeholder
+ */
+int draw_static_textarea(char *placeholder);
+
+/**
+ * Sets up the console for graphical functions
+ */
 void setup_window();
+
+#ifdef __cplusplus
+}
+#endif
