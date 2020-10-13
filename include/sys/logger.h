9406d0c68635e4dcadcfe55d12c7a0182520126e
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-13 16:07:21 +0530

    sys/logger.h: Fix build error

--- include/sys/logger.h
+++ include/sys/logger.h
@@ -8,10 +8,8 @@
 #define _LOGGER_H
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <library/color.h>
 #include <debug.h>
-
-#define LOG(pchar, ...) dbg_printf(pchar, __VA_ARGS__);
-#define ELOG(pchar, ...) dbg_printf(pchar, __VA_ARGS__);
 
 #endif /* A647000C_7AE1_48FA_B3A9_9B6063D808D4 */
