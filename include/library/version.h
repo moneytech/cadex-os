91d0c15a7003bff7f2685ad6836762d83ca85e07
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-15 17:12:36 +0530

    version.h: Update versioning info

--- include/library/version.h
+++ include/library/version.h
@@ -10,11 +10,11 @@
 
 #define COS_SOURCE // Define that this is Cadex-OS source
 
-#define _LTS_REL_ "0.1.7"        // LTS release version; NULL if the version is not
+#define _LTS_REL_ "0.2.0"        // LTS release version; NULL if the version is not
 #define _K_VER_FMT "%d.%d.%d-%s" // This should be printable using the printf() function
-#define __KSHELL_VER_ "1.13.9"   // The Kernel shell version
-#define __VER_SUFFIX "beta"      // Version suffix; Valid values are: beta, prerelease, prebeta, alpha, prealpha
+#define __KSHELL_VER_ "1.15.1"   // The Kernel shell version
+#define __VER_SUFFIX "release"   // Version suffix; Valid values are: beta, prerelease, prebeta, alpha, prealpha
 #define _K_ARCH "i686"           // Kernel architecture
 #define _K_CODENAME "Lean Llama" // Kernel Codename
-#define __K_BUILD_DATE __DATE__ // The date when this kernel was built from source
+#define __K_BUILD_DATE __DATE__ ? __DATE__ : "Not available"  // The date when this kernel was built from source
 #define __K_BUILD_TIME __TIME__ ? __TIME__ : "Not available" // The time when this kernel was built from source
