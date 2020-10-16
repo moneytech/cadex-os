3de9c0ba64559b296cb943a62cdd95c03931bb94
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-16 22:09:50 +0530

    Update ui_window.c

--- libc/truegl/ui_window.c
+++ libc/truegl/ui_window.c
@@ -1,19 +1,12 @@
 #include <truegl/truegl.h>
 
-void create_window(char *title, int border_width, gui_color_t *border_color) {
-    int dims[2];
-    syscall_object_size(WN_STDWINDOW, dims, 2);
+void TGL_window_create(tgl_window_t *window, int x, int y, int w, int h) {
+    window->x = x;
+    window->y = y;
+    window->width = w;
+    window->height = h;
+}
 
-    int width = dims[0];
-    int height = dims[1];
-    char *line[1024];
-    render_window(WN_STDWINDOW);
-    clearScreen(0, 0, width, height);
-    draw_window_border(1, 1, width - 1, height - 1, border_width,
-                       border_color->r, border_color->g, border_color->b);
-    print(10, 10, title);
-    setTextColor(WHITE, 0);
-    drawRect(0, 22, width, 3);
-    flushScreen();
-    flush();
+void TGL_draw_window(tgl_window_t *window){
+    drawRect(window->x, window->y, window->width, window->height);
 }
