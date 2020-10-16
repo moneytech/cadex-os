2b4d0be68b44f6db2ed36d5f68275f2e69a3d40e
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-16 15:09:15 +0530

    Update mouse.h

--- kernel/mouse.h
+++ kernel/mouse.h
@@ -7,43 +7,42 @@ See the file LICENSE for details.
 #ifndef MOUSE_H
 #define MOUSE_H
 
-#include "console.h"
-#include "page.h"
-#include "process.h"
-#include "interrupt.h"
-#include "keyboard.h"
-#include "mouse.h"
-#include "clock.h"
 #include "ata.h"
-#include "device.h"
 #include "cdromfs.h"
-#include "string.h"
+#include "clock.h"
+#include "console.h"
+#include "device.h"
+#include "diskfs.h"
 #include "graphics.h"
+#include "interrupt.h"
 #include "kernel/ascii.h"
 #include "kernel/syscall.h"
-#include "rtc.h"
 #include "kernelcore.h"
+#include "keyboard.h"
 #include "kmalloc.h"
-#include "memorylayout.h"
-#include "kshell.h"
-#include "cdromfs.h"
-#include "diskfs.h"
-#include "serial.h"
-#include <sysinfo.h>
 #include "kprintf.h"
+#include "kshell.h"
+#include "memorylayout.h"
+#include "mouse.h"
+#include "page.h"
+#include "process.h"
+#include "rtc.h"
+#include "serial.h"
 #include "service.h"
+#include "string.h"
+#include <sysinfo.h>
 
-struct mouse_event
-{
-	int32_t x;
-	int32_t y;
-	uint8_t buttons;
+struct mouse_event {
+    int32_t x;
+    int32_t y;
+    uint8_t buttons;
 };
 uint8_t cycle;
 uint8_t buttons;
 int8_t buffer[3];
 
 void mouse_read(struct mouse_event *e);
+void mouse_state(int32_t *x, int32_t *y, uint8_t *buttons);
 void mouse_init();
 float posx, posy;
 float xscale, yscale;
