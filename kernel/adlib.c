18a8f79f617eea729940db3f0b3a4fe8c02119bf
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-16 15:09:29 +0530

    Update adlib.c

--- kernel/adlib.c
+++ kernel/adlib.c
@@ -1,3 +1,13 @@
+/**
+ * Copyright (C) 2019-2020 OpenCreeck
+ * This software is distributed under the GNU General Public License
+ * See the file LICENSE for details
+*/
+
+/**
+ * Yamaha YM3812 (OPL2) sound card driver
+ */
+
 #include "kernel/types.h"
 #include "adlib.h"
 
