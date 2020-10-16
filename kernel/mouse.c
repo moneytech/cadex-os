4e017560f95c80037275f9e0141d5a709a8b11a3
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-16 15:09:17 +0530

    Update mouse.c

--- kernel/mouse.c
+++ kernel/mouse.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2019-2020 OpenCreeck
  * This software is distributed under the GNU General Public License
  * See the file LICENSE for details
-*/
+ */
 
 #include "mouse.h"
 #include "console.h"
@@ -16,28 +16,28 @@
  * The PS2 interface uses a data port and a command port.
  * Reading the command port yields a status byte,
  * while writing to the command port executes commands.
-*/
+ */
 
-#define PS2_DATA_PORT 0x60
+#define PS2_DATA_PORT    0x60
 #define PS2_COMMAND_PORT 0x64
 
 /* Some commands that may be sent to the command port. */
 
-#define PS2_COMMAND_READ_CONFIG 0x20
-#define PS2_COMMAND_WRITE_CONFIG 0x60
-#define PS2_COMMAND_DISABLE_MOUSE 0xA7
-#define PS2_COMMAND_ENABLE_MOUSE 0xA8
+#define PS2_COMMAND_READ_CONFIG      0x20
+#define PS2_COMMAND_WRITE_CONFIG     0x60
+#define PS2_COMMAND_DISABLE_MOUSE    0xA7
+#define PS2_COMMAND_ENABLE_MOUSE     0xA8
 #define PS2_COMMAND_DISABLE_KEYBOARD 0xAD
-#define PS2_COMMAND_ENABLE_KEYBOARD 0xAE
-#define PS2_COMMAND_MOUSE_PREFIX 0xD4
+#define PS2_COMMAND_ENABLE_KEYBOARD  0xAE
+#define PS2_COMMAND_MOUSE_PREFIX     0xD4
 
 /* The status byte read from the command port has these fields. */
 
-#define PS2_STATUS_OBF 0x01  // true: may not write to data port
-#define PS2_STATUS_IBF 0x02  // true: may read from data port
-#define PS2_STATUS_SYS 0x04  // true when port is initialized
-#define PS2_STATUS_A2 0x08   // true if command port was last written to
-#define PS2_STATUS_INH 0x10  // true if keyboard inhibited
+#define PS2_STATUS_OBF  0x01 // true: may not write to data port
+#define PS2_STATUS_IBF  0x02 // true: may read from data port
+#define PS2_STATUS_SYS  0x04 // true when port is initialized
+#define PS2_STATUS_A2   0x08 // true if command port was last written to
+#define PS2_STATUS_INH  0x10 // true if keyboard inhibited
 #define PS2_STATUS_MOBF 0x20 // true if mouse output available
 #define PS2_STATUS_TOUT 0x40 // true if timeout during I/O
 #define PS2_STATUS_PERR 0x80 // true indicates parity error
@@ -46,35 +46,34 @@
  * In addition, a configuration byte may be read/written
  * via PS2_COMMAND_READ/WRITECONFIG.  The configuration byte
  * has these bitfields.
-*/
+ */
 
-#define PS2_CONFIG_PORTA_IRQ 0x01
-#define PS2_CONFIG_PORTB_IRQ 0x02
-#define PS2_CONFIG_SYSTEM 0x04
-#define PS2_CONFIG_RESERVED1 0x08
-#define PS2_CONFIG_PORTA_CLOCK 0x10
-#define PS2_CONFIG_PORTB_CLOCK 0x20
+#define PS2_CONFIG_PORTA_IRQ       0x01
+#define PS2_CONFIG_PORTB_IRQ       0x02
+#define PS2_CONFIG_SYSTEM          0x04
+#define PS2_CONFIG_RESERVED1       0x08
+#define PS2_CONFIG_PORTA_CLOCK     0x10
+#define PS2_CONFIG_PORTB_CLOCK     0x20
 #define PS2_CONFIG_PORTA_TRANSLATE 0x40
-#define PS2_CONFIG_RESERVED2 0x80
+#define PS2_CONFIG_RESERVED2       0x80
 
 /**
  * The mouse has several specialized commands that may
  * be sent by first sending PS2_COMMAND_MOUSE_PREFIX,
  * then one of the following:
-*/
+ */
 
 #define PS2_MOUSE_COMMAND_ENABLE_STREAMING 0xea
-#define PS2_MOUSE_COMMAND_ENABLE_DEVICE 0xf4
-#define PS2_MOUSE_COMMAND_RESET 0xff
+#define PS2_MOUSE_COMMAND_ENABLE_DEVICE    0xf4
+#define PS2_MOUSE_COMMAND_RESET            0xff
 
 /**
  * To read/write data to/from the PS2 port, we must first check
  * for the input/output buffer full (IBF/OBF) bits are set appropriately
  * in the status register.
-*/
+ */
 
-uint8_t ps2_read_data()
-{
+uint8_t ps2_read_data() {
     uint8_t status;
     do {
         status = inb(PS2_COMMAND_PORT);
@@ -82,8 +81,7 @@ uint8_t ps2_read_data()
     return inb(PS2_DATA_PORT);
 }
 
-void ps2_write_data(uint8_t data)
-{
+void ps2_write_data(uint8_t data) {
     uint8_t status;
     do {
         status = inb(PS2_COMMAND_PORT);
@@ -94,10 +92,9 @@ void ps2_write_data(uint8_t data)
 /**
  * In a similar way, to write a command to the status port,
  * we must also check that the IBF field is cleared.
-*/
+ */
 
-void ps2_write_command(uint8_t data)
-{
+void ps2_write_command(uint8_t data) {
     uint8_t status;
     do {
         status = inb(PS2_COMMAND_PORT);
@@ -109,10 +106,9 @@ void ps2_write_command(uint8_t data)
  * Clear the buffer of all data by reading until OBF and IBF
  * are both clear.  Useful when resetting the device to achieve
  * a known state.
-*/
+ */
 
-void ps2_clear_buffer()
-{
+void ps2_clear_buffer() {
     uint8_t status;
     do {
         status = inb(PS2_COMMAND_PORT);
@@ -127,10 +123,9 @@ void ps2_clear_buffer()
  * Send a mouse-specific command by sending the mouse prefix,
  * then the mouse command as data, then reading back an
  * acknowledgement.
-*/
+ */
 
-void ps2_mouse_command(uint8_t command)
-{
+void ps2_mouse_command(uint8_t command) {
     ps2_write_command(PS2_COMMAND_MOUSE_PREFIX);
     ps2_write_data(command);
     ps2_read_data();
@@ -139,16 +134,14 @@ void ps2_mouse_command(uint8_t command)
 /**
  * Read and write the PS2 configuration byte, which
  * does not involve an acknowledgement.
-*/
+ */
 
-uint8_t ps2_config_get()
-{
+uint8_t ps2_config_get() {
     ps2_write_command(PS2_COMMAND_READ_CONFIG);
     return ps2_read_data();
 }
 
-void ps2_config_set(uint8_t config)
-{
+void ps2_config_set(uint8_t config) {
     ps2_write_command(PS2_COMMAND_WRITE_CONFIG);
     ps2_write_data(config);
 }
@@ -161,10 +154,9 @@ static struct mouse_event state;
  * of the X and Y position is given as a single bit in the status
  * word, so we must assemble a twos-complement integer if needed.
  * Finally, take those values and update the current mouse state.
-*/
+ */
 
-static void mouse_interrupt(int i, int code)
-{
+static void mouse_interrupt(int i, int code) {
     uint8_t m1 = inb(PS2_DATA_PORT);
     uint8_t m2 = inb(PS2_DATA_PORT);
     uint8_t m3 = inb(PS2_DATA_PORT);
@@ -181,30 +173,50 @@ static void mouse_interrupt(int i, int code)
         state.x = video_xres - 1;
     if (state.y >= video_yres)
         state.y = video_yres - 1;
-    dbg_printf("[mouse] mouse interrupt fired.\n");
+    switch (state.buttons)
+	{
+	case 1:
+        kprintf("[mouse] left click.\n");
+        break;
+	case 2:
+        kprintf("[mouse] right click.\n");
+        break;
+    default:
+        break;
+	}
 }
 
 /**
  * Do a non-blocking read of the current mouse state.
  * Block interrupts while reading, to avoid inconsistent state.
-*/
+ */
 
-void mouse_read(struct mouse_event* e)
-{
+void mouse_read(struct mouse_event *e) {
     interrupt_disable(44);
     *e = state;
     interrupt_enable(44);
 }
+
+/**
+ * Alternative method to mouse_read()
+ */
+void mouse_state(int32_t *x, int32_t *y, uint8_t *buttons) {
+    interrupt_disable(44);
+    *x = state.x;
+    *y = state.y;
+    *buttons = state.buttons;
+    interrupt_enable(44);
+}
+
 /**
  * Unlike the keyboard, the mouse is not automatically enabled
  * at bootup.  We must first obtain tbe ps2 configuration register,
  * enable both port A (keyboard) and port B (mouse), then send
  * a series of commands to reset the mouse and enable "streaming",
  * which causes an interrupt for every move of the mouse.
-*/
+ */
 
-void mouse_init()
-{
+void mouse_init() {
     ps2_clear_buffer();
 
     uint8_t config = ps2_config_get();
