fea0a4236639938d3e0bffa70a7de5dd32a96d10
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-18 11:57:57 +0530

    Create compositor.c

---
+++ libc/libtgl/compositor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <truegl/events.h>
+#include <truegl/glx.h>
+#include <truegl/truegl.h>
+
+/**
+ * Creates a TWindow with the specified dimensions
+ * @param x X position of the window
+ * @param y Y position of the window
+ * @param w Width of the window
+ * @param h Height of the window
+ * @param visibility Visiblity state of the window. (1 for visible and 0 for invisible)
+ * @param title Title of the window
+ * @returns twindow_t
+ */
+twindow_t window_create(int x, int y, int w, int h, int visibility, char* title)
+{
+    setup_window();
+    twindow_t window;
+    window.properties->y = y;
+    window.properties->x = x;
+    window.properties->visibility = visibility;
+    window.properties->top_level_window = 1;
+    window.properties->height = h;
+    window.properties->width = w;
+    return window;
+}
+
