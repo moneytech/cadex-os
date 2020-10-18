df96c60c7ea432cfb36102bacfcccac63f7206ef
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-18 11:58:30 +0530

    Update abort.c

--- libc/stdio/abort.c
+++ libc/stdio/abort.c
@@ -6,7 +6,7 @@ __attribute__((__noreturn__)) void abort(void)
 {
     // TODO: Abnormally terminate the process as if by SIGABRT.
     printf("abort()\n");
-    dbg_printf("[PANIC(stdio.h:9:4)]: abort() called by a userspace program\n");
+    dbg_printf("abort(): called by a userspace program\n");
     while (1)
     {
     }
