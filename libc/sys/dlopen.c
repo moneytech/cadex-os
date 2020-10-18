22511322ceda049655bf7596d8d15afc3e495174
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-18 14:20:06 +0530

    Create dlopen.c

---
+++ libc/sys/dlopen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/module.h>
+
+int dlopen(const char *path, dlib_t *dlib){
+	// TODO: Implement the module functionality
+    dbg_printf("dlopen(): not implemented\n");
+    return 0;
+}
