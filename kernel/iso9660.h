01062990588c282d30d717ee5b4e5b1d035ab0ae
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-10-22 12:48:54 +0530

    Update iso9660.h

--- kernel/iso9660.h
+++ kernel/iso9660.h
@@ -1,8 +1,9 @@
-/*
-Copyright (C) 2019-2020 The CadexOS Project
-This software is distributed under the GNU General Public License.
-See the file LICENSE for details.
-*/
+/**
+ * Copyright (C) 2019-2020 The CadexOS Project
+ * This software is distributed under the GNU General Public License
+ * See the file LICENSE for details
+ */
+
 
 #ifndef ISO9660_H
 #define ISO9660_H
