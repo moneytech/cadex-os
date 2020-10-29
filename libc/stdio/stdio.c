4a0d4464f7baec406e68f77c0b8df8bb59eeeeb9
asiawin <aswindeepak40@gmail.com>
2020-10-29 19:51:28 +0530

    stdio: Removed redundant functions

--- libc/stdio/stdio.c
+++ libc/stdio/stdio.c
@@ -157,29 +157,10 @@ void print(int x, int y, char *s) {
 
 int puts(const char *s) {
     /* eof? */
-    printf(s);
+    printf("%s", s);
     return 0;
 }
 
-// Prints text on screen on the specified x and y axis.
-void print_debug(int x, int y, char *s) {
-#ifdef DEBUG
-    draw_set_buffer(GRAPHICS_TEXT, x, y, (int)s, 0);
-    flushScreen();
-#endif // DEBUG
-}
-
-// Get data from specified port.
-uint8_t inb(uint16_t port) {
-    uint8_t data;
-    asm volatile("inb %1, %0" : "=a"(data) : "Nd"(port));
-    return data;
-}
-
-// Send data to specified port.
-void outb(uint16_t port, uint8_t data) {
-    asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
-}
 void wait_for_io(uint32_t timer_count) {
     while (1) {
         asm volatile("nop");
@@ -189,6 +170,7 @@ void wait_for_io(uint32_t timer_count) {
     }
 }
 
+// Prints to the stdio buffer
 static void printf_buffer(char *s, unsigned len) {
     while (len) {
         unsigned l = len % (PAGE_SIZE - 1);
@@ -203,6 +185,7 @@ static void printf_buffer(char *s, unsigned len) {
     stdio_buffer[stdio_buffer_index] = 0;
 }
 
+// Use putc instead
 void printf_putchar(char c) {
     printf_buffer(&c, 1);
     if (c == '\n')
@@ -212,71 +195,9 @@ void printf_putchar(char c) {
 
 void printf_putstring(char *s) { printf_buffer(s, strlen(s)); }
 
-// Sets the CPU to noop mode for specified milliseconds
-void sleep(uint32_t timer_count) { wait_for_io(timer_count * 0x02FFFFFF); }
-
-// Play sound with specified frequency
-static void play_sound(uint32_t nFrequence) {
-    uint32_t Div;
-    uint8_t tmp;
-
-    // Set the PIT to the desired frequency
-    Div = 1193180 / nFrequence;
-    outb(0x43, 0xb6);
-    outb(0x42, (uint8_t)(Div));
-    outb(0x42, (uint8_t)(Div >> 8));
-
-    // And play the sound using the PC speaker
-    tmp = inb(0x61);
-    if (tmp != (tmp | 3)) {
-        outb(0x61, tmp | 3);
-    }
-}
-
-// Stop playing sound
-static void nosound() {
-    uint8_t tmp = inb(0x61) & 0xFC;
-
-    outb(0x61, tmp);
-}
-
-// A code for Beeping. May not work correctly on QEMU. See
-// https://wiki.osdev.org/PC_Speaker#Sample_Code/
-void beep() {
-    play_sound(1000);
-    sleep(10);
-    nosound();
-    // set_PIT_2(old_frequency);
-}
-// End sound code
-
-// Returns Window Dimensions. EXPERIMENTAL
-int getWindowDimens(char *type) {
-    int dims[2];
-    syscall_object_size(WN_STDWINDOW, dims, 2);
-
-    int width = dims[0];
-    int height = dims[1];
-    if (!strcmp(type[0], "width")) {
-        return width;
-    }
-    if (!strcmp(type[0], "height")) {
-        return height;
-    }
-    return width, height;
-}
-
 // Executes specified file with specified argc and argv.
-void system(char *program, int argc, char **argv) { exec(program, argc, argv); }
+void system(char *program, int argc, const char **argv) { exec(program, argc, argv); }
 
-void __stdio_init_buffers(void) {
-    _stdin.read_buf = malloc(BUFSIZ);
-    //_stdout.read_buf = malloc(BUFSIZ);
-    //_stderr.read_buf = malloc(BUFSIZ);
-    _stdin._name = strdup("stdin");
-    _stdout._name = strdup("stdout");
-    _stderr._name = strdup("stderr");
-}
 int setvbuf(FILE *stream, char *buf, int mode, size_t size) {
     if (mode != _IOLBF) {
         return -1; /* Unsupported */
@@ -317,11 +238,8 @@ int setvbuf(FILE *stream, char *buf, int mode, size_t size) {
 
 // 	return out;
 // }
-/**
- * Draws a border with the specified dimensions and color
- * @param x
- * @param y
- */
+
+// Draws a border with the specified dimensions and color
 void draw_window_border(int x, int y, int w, int h, int thickness, int r, int g,
                         int b) {
     setTextColor(r, b, g, 0);
@@ -334,36 +252,12 @@ void draw_window_border(int x, int y, int w, int h, int thickness, int r, int g,
     // resetColor();
 }
 
-/**
- * Draw the Cadex OS Logo to the console
- * @param x The x position of logo
- * @param y The y position of the logo
- */
-void draw_cadex_logo(int x, int y) {
-    render_window(WN_STDWINDOW);
-    drawRect(x, y, 50, 50);
-    setTextColor(GREEN, 0);
-    drawRect(x, y, 40, 40);
-    setTextColor(CLEAR_RED, 0);
-    drawRect(x, y, 30, 30);
-    setTextColor(BLUE, 0);
-    drawRect(x, y, 20, 20);
-    setTextColor(GRAY, 0);
-    drawRect(x, y, 10, 10);
-    flushScreen();
-    flush();
-}
-
-/**
- * Get a line from a file
- * @param fd The file to read
- * @param buffer The buffer to save to. (Probably a char *[])
- *
- */
+// Get a line from a file
 int fgets(int fd, char *buffer[], int length) {
     return read_object(fd, buffer, length);
 }
 
+// Get char
 uint8_t getch() {
     uint8_t data;
     read_object(STDIN, &data, 1);
