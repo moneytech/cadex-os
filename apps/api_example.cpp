d7944c154b096ca53aa1a9838df989a29e992f54
asiawin <aswindeepak40@gmail.com>
2020-10-29 20:16:09 +0530

    Apps: Fix formatting for api_example.cpp

--- apps/api_example.cpp
+++ apps/api_example.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2019-2020 The CadexOS Project
  * This software is distributed under the GNU General Public License
  * See the file LICENSE for details
-*/
+*/ 
 
 #include <fstream>
 #include <iostream>
